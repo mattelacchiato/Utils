c0c5748cfb03a29592a3fe09dee7d26ccc255f64
mattelacchiato <mb@blau.de>
2013-03-20 11:57:58 +0100

    added date methods

--- src/de/splitstudio/utils/DateUtils.java
+++ src/de/splitstudio/utils/DateUtils.java
@@ -1,18 +1,22 @@
 package de.splitstudio.utils;
 
 import java.util.Calendar;
-import java.util.Locale;
+import java.util.Date;
 
 public class DateUtils {
 
-	public static Calendar createFirstDayOfYear(Locale locale) {
-		Calendar cal = createFirstDayOfMonth(locale);
+	public static boolean isBetween(Date start, Date now, Date end) {
+		return (start == null || !start.after(now)) && (end == null || !end.before(now));
+	}
+
+	public static Calendar createFirstDayOfYear() {
+		Calendar cal = createFirstDayOfMonth();
 		cal.set(Calendar.MONTH, 0);
 		return cal;
 	}
 
-	public static Calendar createFirstDayOfMonth(Locale locale) {
-		Calendar cal = Calendar.getInstance(locale);
+	public static Calendar createFirstDayOfMonth() {
+		Calendar cal = Calendar.getInstance();
 		cal.set(Calendar.DAY_OF_MONTH, 1);
 		cal.set(Calendar.HOUR_OF_DAY, 0);
 		cal.set(Calendar.MINUTE, 0);
--- test/de/splitstudio/utils/DateUtilsTest.java
+++ test/de/splitstudio/utils/DateUtilsTest.java
@@ -13,57 +13,149 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.util.Calendar;
-import java.util.Locale;
+import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class DateUtilsTest {
 
-	private static final Locale LOCALE = Locale.GERMANY;
 	private Calendar now;
 
 	@Before
 	public void setUp() {
-		now = Calendar.getInstance(LOCALE);
+		now = Calendar.getInstance();
 	}
 
 	@Test
 	public void createFirstDayOfMonth_yearIsSet() {
-		assertThat(createFirstDayOfMonth(LOCALE).get(YEAR), is(now.get(YEAR)));
+		assertThat(createFirstDayOfMonth().get(YEAR), is(now.get(YEAR)));
 	}
 
 	@Test
 	public void createFirstDayOfMonth_monthIsSet() {
-		assertThat(createFirstDayOfMonth(LOCALE).get(MONTH), is(now.get(MONTH)));
+		assertThat(createFirstDayOfMonth().get(MONTH), is(now.get(MONTH)));
 	}
 
 	@Test
 	public void createFirstDayOfMonth_dayIsOne() {
-		assertThat(createFirstDayOfMonth(LOCALE).get(DAY_OF_MONTH), is(1));
+		assertThat(createFirstDayOfMonth().get(DAY_OF_MONTH), is(1));
 	}
 
 	@Test
 	public void createFirstDayOfMonth_OthersAreZero() {
-		assertThat(createFirstDayOfMonth(LOCALE).get(HOUR_OF_DAY), is(0));
-		assertThat(createFirstDayOfMonth(LOCALE).get(MINUTE), is(0));
-		assertThat(createFirstDayOfMonth(LOCALE).get(SECOND), is(0));
-		assertThat(createFirstDayOfMonth(LOCALE).get(MILLISECOND), is(0));
+		assertThat(createFirstDayOfMonth().get(HOUR_OF_DAY), is(0));
+		assertThat(createFirstDayOfMonth().get(MINUTE), is(0));
+		assertThat(createFirstDayOfMonth().get(SECOND), is(0));
+		assertThat(createFirstDayOfMonth().get(MILLISECOND), is(0));
 	}
 
 	@Test
 	public void createFirstDayOfYear_yearIsSet() {
-		assertThat(DateUtils.createFirstDayOfYear(LOCALE).get(YEAR), is(now.get(YEAR)));
+		assertThat(DateUtils.createFirstDayOfYear().get(YEAR), is(now.get(YEAR)));
 	}
 
 	@Test
 	public void createFirstDayOfYear_OthersAreZero() {
-		assertThat(createFirstDayOfYear(LOCALE).get(MONTH), is(0));
-		assertThat(createFirstDayOfYear(LOCALE).get(DAY_OF_MONTH), is(1));
-		assertThat(createFirstDayOfYear(LOCALE).get(HOUR_OF_DAY), is(0));
-		assertThat(createFirstDayOfYear(LOCALE).get(MINUTE), is(0));
-		assertThat(createFirstDayOfYear(LOCALE).get(SECOND), is(0));
-		assertThat(createFirstDayOfYear(LOCALE).get(MILLISECOND), is(0));
+		assertThat(createFirstDayOfYear().get(MONTH), is(0));
+		assertThat(createFirstDayOfYear().get(DAY_OF_MONTH), is(1));
+		assertThat(createFirstDayOfYear().get(HOUR_OF_DAY), is(0));
+		assertThat(createFirstDayOfYear().get(MINUTE), is(0));
+		assertThat(createFirstDayOfYear().get(SECOND), is(0));
+		assertThat(createFirstDayOfYear().get(MILLISECOND), is(0));
+	}
+
+	@Test
+	public void isBetween_isBetween_true() {
+		Calendar cal = Calendar.getInstance();
+		Date start = cal.getTime();
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		Date now = cal.getTime();
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		Date end = cal.getTime();
+		assertThat(DateUtils.isBetween(start, now, end), is(true));
+	}
+
+	@Test
+	public void isBetween_startNull_true() {
+		Calendar cal = Calendar.getInstance();
+		Date start = null;
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		Date now = cal.getTime();
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		Date end = cal.getTime();
+		assertThat(DateUtils.isBetween(start, now, end), is(true));
+	}
+
+	@Test
+	public void isBetween_endNull_true() {
+		Calendar cal = Calendar.getInstance();
+		Date start = cal.getTime();
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		Date now = cal.getTime();
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		Date end = null;
+		assertThat(DateUtils.isBetween(start, now, end), is(true));
+	}
+
+	@Test
+	public void isBetween_StartAndEndNull_true() {
+		Calendar cal = Calendar.getInstance();
+		Date start = null;
+		Date now = cal.getTime();
+		Date end = null;
+		assertThat(DateUtils.isBetween(start, now, end), is(true));
+	}
+
+	@Test
+	public void isBetween_nowEqualsStart_true() {
+		Calendar cal = Calendar.getInstance();
+		Date start = cal.getTime();
+		Date now = cal.getTime();
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		Date end = cal.getTime();
+		assertThat(DateUtils.isBetween(start, now, end), is(true));
+	}
+
+	@Test
+	public void isBetween_nowEqualsEnd_true() {
+		Calendar cal = Calendar.getInstance();
+		Date start = cal.getTime();
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		Date now = cal.getTime();
+		Date end = cal.getTime();
+		assertThat(DateUtils.isBetween(start, now, end), is(true));
+	}
+
+	@Test
+	public void isBetween_nowEqualsStartAndEnd_true() {
+		Calendar cal = Calendar.getInstance();
+		Date start = cal.getTime();
+		Date now = cal.getTime();
+		Date end = cal.getTime();
+		assertThat(DateUtils.isBetween(start, now, end), is(true));
+	}
+
+	@Test
+	public void isBetween_nowBeforeStart_false() {
+		Calendar cal = Calendar.getInstance();
+		Date now = cal.getTime();
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		Date start = cal.getTime();
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		Date end = cal.getTime();
+		assertThat(DateUtils.isBetween(start, now, end), is(false));
+	}
+
+	@Test
+	public void isBetween_nowAfterEnd_false() {
+		Calendar cal = Calendar.getInstance();
+		Date start = cal.getTime();
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		Date end = cal.getTime();
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		Date now = cal.getTime();
+		assertThat(DateUtils.isBetween(start, now, end), is(false));
 	}
 
 }
