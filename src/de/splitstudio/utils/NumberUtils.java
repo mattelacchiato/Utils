844861dce110297e436527a8ff3282449f90a8e1
mattelacchiato <mb@blau.de>
2013-09-08 17:41:13 +0200

    no locale needed. we allways take the default anyway.

--- src/de/splitstudio/utils/NumberUtils.java
+++ src/de/splitstudio/utils/NumberUtils.java
@@ -2,30 +2,31 @@ package de.splitstudio.utils;
 
 import java.text.NumberFormat;
 import java.text.ParseException;
-import java.util.Locale;
 
 public class NumberUtils {
 
 	private static final double CENT_FACTOR = 100;
 
-	private static final NumberFormat decimalFormat = NumberFormat.getNumberInstance();
+	private static NumberFormat currencyFormat;
+	private static NumberFormat decimalFormat;
+
 	static {
+		init();
+	}
+
+	static void init() {
+		currencyFormat = NumberFormat.getCurrencyInstance();
+		decimalFormat = NumberFormat.getNumberInstance();
 		decimalFormat.setMinimumFractionDigits(2);
 		decimalFormat.setMaximumFractionDigits(2);
 	}
-
-	//TODO (07.09.2013): put numberinstance as static field
 
 	public static String formatAsDecimal(int cents) {
 		return decimalFormat.format(centToDouble(cents));
 	}
 
-	public static String formatAsCurrency(int cents, Locale locale) {
-		return NumberFormat.getCurrencyInstance(locale).format(centToDouble(cents));
-	}
-
 	public static String formatAsCurrency(int cents) {
-		return formatAsCurrency(cents, Locale.getDefault());
+		return currencyFormat.format(centToDouble(cents));
 	}
 
 	public static double centToDouble(int cents) {
@@ -33,14 +34,7 @@ public class NumberUtils {
 	}
 
 	public static int parseCent(String amount) throws ParseException {
-		return parseCent(amount, Locale.getDefault());
-	}
-
-	//TODO 02.06.2013: use qualifiers to test locale specific stuff!
-	//@Test @Values(qualifiers = "fr-land")
-
-	public static int parseCent(String amount, Locale locale) throws ParseException {
-		return (int) (NumberFormat.getNumberInstance(locale).parse(amount).doubleValue() * CENT_FACTOR);
+		return (int) (decimalFormat.parse(amount).doubleValue() * CENT_FACTOR);
 	}
 
 }
--- test/de/splitstudio/utils/NumberUtilsTest.java
+++ test/de/splitstudio/utils/NumberUtilsTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertThat;
 
 import java.util.Locale;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -12,56 +14,83 @@ import org.robolectric.RobolectricTestRunner;
 @RunWith(RobolectricTestRunner.class)
 public class NumberUtilsTest {
 
-	//TODO (07.09.2013): rm Locales from parameters and set them via Locale.setDefault()
+	private Locale systemDefault;
+
+	@Before
+	public void setUp() {
+		systemDefault = Locale.getDefault();
+		Locale.setDefault(Locale.US);
+		NumberUtils.init();
+	}
+
+	@After
+	public void tearDown() {
+		Locale.setDefault(systemDefault);
+		NumberUtils.init();
+	}
 
 	@Test
 	public void formatAsCurrency_formats1Dollar() {
-		assertThat(NumberUtils.formatAsCurrency(100, Locale.US), is("$1.00"));
+		assertThat(NumberUtils.formatAsCurrency(100), is("$1.00"));
 	}
 
 	@Test
 	public void formatAsCurrency_formatsMinus1Dollar() {
-		assertThat(NumberUtils.formatAsCurrency(-100, Locale.US), is("($1.00)"));
+		assertThat(NumberUtils.formatAsCurrency(-100), is("($1.00)"));
 	}
 
 	@Test
 	public void formatAsCurrency_formats1Euro() {
-		assertThat(NumberUtils.formatAsCurrency(100, Locale.GERMANY), is("1,00 €"));
+		setGermanLocale();
+		assertThat(NumberUtils.formatAsCurrency(100), is("1,00 €"));
 	}
 
 	@Test
 	public void formatAsCurrency_formatsMinus1Euro() {
-		assertThat(NumberUtils.formatAsCurrency(-100, Locale.GERMANY), is("-1,00 €"));
+		setGermanLocale();
+		assertThat(NumberUtils.formatAsCurrency(-100), is("-1,00 €"));
 	}
 
 	@Test
 	public void parseCent_1dollar() throws Exception {
-		assertThat(NumberUtils.parseCent("1.00", Locale.US), is(100));
+		assertThat(NumberUtils.parseCent("1.00"), is(100));
 	}
 
 	@Test
 	public void parseCent_minus1dollar() throws Exception {
-		assertThat(NumberUtils.parseCent("-1.00", Locale.US), is(-100));
+		assertThat(NumberUtils.parseCent("-1.00"), is(-100));
 	}
 
 	@Test
 	public void parseCent_minus1cent() throws Exception {
-		assertThat(NumberUtils.parseCent("-0.01", Locale.US), is(-1));
+		assertThat(NumberUtils.parseCent("-0.01"), is(-1));
 	}
 
 	@Test
 	public void parseCent_1000Euro() throws Exception {
-		assertThat(NumberUtils.parseCent("1000", Locale.GERMAN), is(100000));
+		setGermanLocale();
+		assertThat(NumberUtils.parseCent("1000"), is(100000));
 	}
 
 	@Test
 	public void parseCent_minus123_456Euro() throws Exception {
-		assertThat(NumberUtils.parseCent("-123,456€", Locale.GERMAN), is(-12345));
+		setGermanLocale();
+		assertThat(NumberUtils.parseCent("-123,456€"), is(-12345));
 	}
 
 	@Test
 	public void formatAsDecimal_100_1dot00() {
-		Locale.setDefault(Locale.US);
 		assertThat(NumberUtils.formatAsDecimal(100), is("1.00"));
+	}
+
+	@Test
+	public void formatAsDecimal_100toGermany_1comma00() {
+		setGermanLocale();
+		assertThat(NumberUtils.formatAsDecimal(100), is("1,00"));
+	}
+
+	private void setGermanLocale() {
+		Locale.setDefault(Locale.GERMANY);
+		NumberUtils.init();
 	}
 }
